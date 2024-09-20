1f80662847ae62d332c9bd46c2eb11abe7c0d018
junha03 <angry03@uos.ac.kr>
2024-09-20 14:26:02 +0900

    add healthcheck

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,3 +4,7 @@ import defaultRouter from "./default";
 
 Vars.app.use("/api", gameRouter);
 Vars.app.use("/api", defaultRouter);
+
+Vars.app.get("/healthcheck", (req, res) => {
+    res.send("OK");
+});
